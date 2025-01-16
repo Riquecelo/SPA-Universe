53eb8718484a1beb143ff7e7c633411d59da2423
Marcelo Santos <marcelo.has@hotmail.com>
2025-01-16 01:31:53 -0300

    inicializando node e lite-server

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ import {Router} from './router.js'
 
 const router = new Router()
 router.add('/', 'pages/home.html')
-router.add('/about', '/pages/about.html')
-router.add('/contact', 'pages/contact.html')
+router.add('/universo', '/pages/universo.html')
+router.add('/exploracao', 'pages/exploracao.html')
 router.add(404, 'pages/404.html')
 
 
